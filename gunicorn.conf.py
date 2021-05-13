4e76a09e3347659d9ad0f5d56f4bd47edebef7ed
Cloud11665 <Cloud11665@gmail.com>
2021-05-14 00:54:31 +0200

    add gunicorn config
    add  guard

---
+++ gunicorn.conf.py
@@ -0,0 +1,5 @@
+bind = "127.0.0.1:7001"
+workers = 1 #>1 breaks cache
+worker_connections = 1000
+timeout = 30
+keepalive = 2
--- main.py
+++ main.py
@@ -69,4 +69,5 @@ def projects():
 		,selected="/projects"
 	)
 
-app.run(debug=True)
+if __name__ == "__main__":
+	app.run(debug=True)
